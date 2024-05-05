0158949ae1b6cc305c721565d062634c9b5d7966
Oscar Roche <oscar.w.roche@gmail.com>
2024-05-05 18:30:21 +0200

    :label: change image to a union type for lifecycle

--- src/domain/repositories/image_repository.rs
+++ src/domain/repositories/image_repository.rs
@@ -1,8 +1,8 @@
-use crate::domain::entities::image::Image;
+use crate::domain::entities::image::{GeneratedImage, SavedImage};
 use axum::async_trait;
 use std::error::Error;
 
 #[async_trait]
 pub trait ImageRepository {
-    async fn save(&self, image: &Image) -> Result<(), Box<dyn Error>>; // Error handling simplified
+    async fn save(&self, image: &GeneratedImage) -> Result<SavedImage, Box<dyn Error>>; // Error handling simplified
 }
--- src/domain/services/image_generation_service.rs
+++ src/domain/services/image_generation_service.rs
@@ -2,9 +2,12 @@ use std::error::Error;
 
 use axum::async_trait;
 
-use crate::domain::entities::image::Image;
+use crate::domain::entities::image::GeneratedImage;
 
 #[async_trait]
 pub trait ImageGenerationService {
-    async fn generate_image_from_prompt(&self, prompt: &str) -> Result<Image, Box<dyn Error>>;
+    async fn generate_image_from_prompt(
+        &self,
+        prompt: &str,
+    ) -> Result<GeneratedImage, Box<dyn Error>>;
 }
