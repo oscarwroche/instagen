68db8d9e69686ece69dbc38dc616071efb307b3e
Oscar Roche <oscar.w.roche@gmail.com>
2024-05-06 10:22:53 +0200

    :label: add delete method to repo trait

--- src/domain/repositories/image_repository.rs
+++ src/domain/repositories/image_repository.rs
@@ -4,5 +4,6 @@ use std::error::Error;
 
 #[async_trait]
 pub trait ImageRepository {
-    async fn save(&self, image: &GeneratedImage) -> Result<SavedImage, Box<dyn Error>>; // Error handling simplified
+    async fn save(&self, image: &GeneratedImage) -> Result<SavedImage, Box<dyn Error>>;
+    async fn delete(&self, image_id: String) -> Result<(), Box<dyn Error>>;
 }
